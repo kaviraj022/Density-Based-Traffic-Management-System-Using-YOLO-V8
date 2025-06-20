de6b135062b383b842558249eeb735b4ccac0ed0
kaviraj022 <ikavikr8098@gmail.com>
2025-06-20 11:52:01 +0530

    Add upload page for 4 lane videos/images and media file handling

--- traffic_signal/traffic/views.py
+++ traffic_signal/traffic/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
 
-# Create your views here.
+LANES = ['north', 'south', 'east', 'west']
+
+def upload_view(request):
+    if request.method == 'POST':
+        files = {}
+        for lane in LANES:
+            uploaded = request.FILES.get(lane)
+            if uploaded:
+                fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, lane))
+                filename = fs.save(uploaded.name, uploaded)
+                files[lane] = fs.url(filename)
+        request.session['lane_files'] = files
+        return redirect('upload')  # For now, just reload the page
+    return render(request, 'traffic/upload.html')
