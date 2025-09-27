570aa8cb3c84aee0c04a1a29e0a154eeb060bbcd
kaviraj022 <ikavikr8098@gmail.com>
2025-09-27 09:36:14 +0530

    Removed Django setup

--- traffic_signal/traffic/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.upload_view, name='upload'),
-    path('intersection/', views.intersection_view, name='intersection'),
-] 
